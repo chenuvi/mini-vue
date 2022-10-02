43c85ca8410cd5891a0d75a1a417d353f79b8f7b
chenuvi <chenuvi@outlook.com>
2022-10-02 15:54:39 +0800

    Feat: effect reactive

---
+++ src/test/effect.spec.ts
@@ -0,0 +1,21 @@
+import { effect } from '../effect'
+import { reactive } from '../reactive'
+
+describe("effect", () => {
+  it("effect happy path", () => {
+    const user = reactive({
+      name: 'sys',
+      age: 18
+    })
+
+    let nextAge
+    effect(() => {
+      nextAge = user.age + 1
+    })
+    expect(nextAge).toBe(19)
+
+    // update
+    user.age++
+    expect(nextAge).toBe(20)
+  })
+})
