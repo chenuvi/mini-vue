6791b8cd871a3ec93fcf9d2d2650ed979a35e8a5
chenuvi <chenuvi@outlook.com>
2022-10-03 22:40:31 +0800

    TODO: effect  scheduler

--- src/effect.ts
+++ src/effect.ts
@@ -1,7 +1,7 @@
 class ReactiveEffect {
   private _fn: any
 
-  constructor(fn) {
+  constructor(fn, public scheduler?) {
     this._fn = fn
   }
   run() {
@@ -29,14 +29,19 @@ export const trigger = (target, key) => {
   let dep = depsMap.get(key)
 
   for (const effect of dep) {
-    effect.run()
+    if (effect.scheduler) {
+      effect.scheduler()
+    } else {
+      effect.run()
+    }
   }
 }
 // 用一个全局变量存储被注册的副作用函数
 let activeEffect
-export const effect = (fn) => {
+export const effect = (fn, options: any = {}) => {
+  const { scheduler } = options
   // 这里存起来跑一下触发reactive的get
-  const _effect = new ReactiveEffect(fn)
+  const _effect = new ReactiveEffect(fn, scheduler)
   _effect.run()
   return _effect.run.bind(_effect)
 }
--- src/test/effect.spec.ts
+++ src/test/effect.spec.ts
@@ -2,33 +2,29 @@ import { effect } from '../effect'
 import { reactive } from '../reactive'
 
 describe("effect", () => {
-  it("effect happy path", () => {
-    const user = reactive({
-      name: 'sys',
-      age: 18
+  it('scheduler', () => {
+    let dummy
+    let run: any
+    const scheduler = jest.fn(() => {
+      run = runner
     })
-
-    let nextAge
-    effect(() => {
-      nextAge = user.age + 1
-    })
-    expect(nextAge).toBe(19)
-
-    // update
-    user.age++
-    expect(nextAge).toBe(20)
-  })
-
-  it("should return runner when call effect", () => {
-    // 1. effect(fn) => function (runner) => fn => return
-    let foo = 10
-    const runner = effect(() => {
-      foo++
-      return "foo"
-    })
-    expect(foo).toBe(11)
-    const res = runner()
-    expect(foo).toBe(12)
-    expect(res).toBe("foo")
+    const obj = reactive({ foo: 1 })
+    const runner = effect(
+      () => {
+        dummy = obj.foo
+      },
+      { scheduler }
+    )
+    expect(scheduler).not.toHaveBeenCalled()
+    expect(dummy).toBe(1)
+    // should be called on first trigger
+    obj.foo++
+    expect(scheduler).toHaveBeenCalledTimes(1)
+    // should not run yet
+    expect(dummy).toBe(1)
+    // manually run
+    run()
+    // should have run
+    expect(dummy).toBe(2)
   })
 })
